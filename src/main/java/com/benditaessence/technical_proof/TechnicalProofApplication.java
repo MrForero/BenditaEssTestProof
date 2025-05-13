3043b9432f7235f8760a16162589f01cacb9541b
MrForero <foreroyatesantiago@gmail.com>
2025-05-12 19:03:20 -0500

    first commit

---
+++ src/main/java/com/benditaessence/technical_proof/TechnicalProofApplication.java
@@ -0,0 +1,13 @@
+package com.benditaessence.technical_proof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalProofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalProofApplication.class, args);
+	}
+
+}
